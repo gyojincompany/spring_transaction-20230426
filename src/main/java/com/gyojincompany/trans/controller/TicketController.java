4fd7b016a7706caf2ab7e5185cf1ffbee381a202
ICI <ICI@DESKTOP-EA5M9P6>
2023-04-26 15:28:01 +0900

    transaction 기능 추가

---
+++ src/main/java/com/gyojincompany/trans/controller/TicketController.java
@@ -0,0 +1,43 @@
+package com.gyojincompany.trans.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gyojincompany.trans.dao.TicketDao;
+import com.gyojincompany.trans.dto.CardDto;
+
+@Controller
+public class TicketController {
+	
+	private TicketDao dao;
+	
+	@Autowired
+	public void setDao(TicketDao dao) {
+		this.dao = dao;
+	}
+	
+	
+	
+	@RequestMapping(value = "/buy_ticket")
+	public String buy_ticket() {
+		
+		return "buyTicket";
+	}	
+	
+
+	@RequestMapping(value="/buyTicketOk")
+	public String buy_ticketOk(CardDto cardDto, Model model) {
+		
+//		TicketDao dao = new TicketDao();
+//		dao.buyTicket(dto);
+		
+		dao.buyTicket(cardDto);
+		
+		model.addAttribute("ticketInfo", cardDto);
+		
+		return "buyTicketOk";
+	}
+
+}
